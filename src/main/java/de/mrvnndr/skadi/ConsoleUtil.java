553a025d93af2b81f5cb5c0e3cae3052d2dee4f2
mrvnndr <marvin.ender@udo.edu>
2024-05-05 06:06:04 +0200

    Deactivate color output depending on env vars.

--- src/main/java/de/mrvnndr/skadi/ConsoleUtil.java
+++ src/main/java/de/mrvnndr/skadi/ConsoleUtil.java
@@ -10,17 +10,32 @@ public class ConsoleUtil {
     }
 
     public static void print(String str, Color color) {
-        System.out.print("Skadi -- ");
-        System.out.print(color.escape);
-        System.out.print(str);
-        System.out.print("\033[0m");
+        System.err.print("Skadi -- ");
+        doWithColor(color, () -> System.err.print(str));
     }
 
     public static void println(String str, Color color) {
-        System.out.print("Skadi -- ");
-        System.out.print(color.escape);
-        System.out.println(str);
-        System.out.print("\033[0m");
+        System.err.print("Skadi -- ");
+        doWithColor(color, () -> System.err.println(str));
+    }
+
+    private static void doWithColor(Color color, Runnable action) {
+        var useColor = shouldUseColor();
+        if (useColor) {
+            System.err.print(color.escape);
+        }
+        action.run();
+        if (useColor) {
+            System.err.print("\033[0m");
+        }
+    }
+
+    public static boolean shouldUseColor() {
+        var noColorVar = System.getenv("NO_COLOR");
+        if (noColorVar != null && !noColorVar.isEmpty()) {
+            return false;
+        }
+        return !"dumb".equals(System.getenv("TERM"));
     }
 
     public enum Color {
